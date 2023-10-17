3aafa17bd17acbc1c27508c3085475082f1076f1
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-10-17 19:30:21 +0200

    1 part

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,5 +1,7 @@
 package example.cashcard;
 
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,12 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
+	private CashCardRepository cashCardRepository;
+	
+	public CashCardController(CashCardRepository cashCardRepository) {
+		this.cashCardRepository = cashCardRepository;
+	}
 	
 	@GetMapping("/{requestId}")
 	public ResponseEntity<CashCard> findById(@PathVariable Long requestId) {
-		if(requestId.equals(99L)) {
-		CashCard cashCard = new CashCard(99L, 123.45);
-		return ResponseEntity.ok(cashCard);
+		Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestId);
+//		if(requestId.equals(99L)) {
+//		CashCard cashCard = new CashCard(99L, 123.45);
+//		return ResponseEntity.ok(cashCard);
+		if(cashCardOptional.isPresent()) {
+			return ResponseEntity.ok(cashCardOptional.get());
 		} else {
 			return ResponseEntity.notFound().build();
 		}

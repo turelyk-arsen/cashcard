02c01d47122c0fa976f6100516f6f1bfa5370c4a
turelyk-arsen <114808096+turelyk-arsen@users.noreply.github.com>
2023-10-18 18:00:31 +0200

    add pagination and sort

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,15 +1,14 @@
 package example.cashcard;
 
 import java.net.URI;
-import java.util.Optional;
+import java.util.*;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
@@ -42,6 +41,17 @@ public class CashCardController {
 				.toUri();
 		
 		return ResponseEntity.created(locationOfNewCashCard).build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+		Page<CashCard> page = cashCardRepository
+				.findAll(PageRequest.of
+						(pageable.getPageNumber(),
+						pageable.getPageSize(),
+						pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))));
+				
+		return ResponseEntity.ok(page.getContent());
 	}
 
 }
